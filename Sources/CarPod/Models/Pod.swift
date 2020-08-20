e901629d6f10db972788080bd6958ba225ec2a79
zhvrnkov <vlad.zhavoronkov@rosberry.com>
2020-08-20 15:25:17 +0600

    Remove / from Pod.name

--- Sources/CarPod/Models/Pod.swift
+++ Sources/CarPod/Models/Pod.swift
@@ -5,6 +5,21 @@
 import Foundation
 
 struct Pod: Codable {
+
+    private enum CodingKeys: String, CodingKey {
+        case name = "name"
+        case version
+    }
+
     let name: String
     let version: Version?
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let slashCharacter = Character("/")
+        name = try container.decode(String.self, forKey: .name).filter { character in
+            character != slashCharacter
+        }
+        version = try container.decodeIfPresent(Version.self, forKey: .version)
+    }
 }
