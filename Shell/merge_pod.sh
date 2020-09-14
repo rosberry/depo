ced339025ba4e9ea12c2f730cb87103caa3c4d11
zhvrnkov <vlad.zhavoronkov@rosberry.com>
2020-09-14 10:19:59 +0600

    Fix merge_pod

--- Shell/merge_pod.sh
+++ Shell/merge_pod.sh
@@ -8,10 +8,10 @@ PRODUCT_NAME=$2
 OUTPUT_PATH="./Build/iOS/${PRODUCT_NAME}.framework"
 
 # Copy the device version of framework to OUTPUT_PATH.
-IPHONEOS_FRAMEWORK_DIR = "../build/Release-iphoneos/${SCHEMA_NAME}/${PRODUCT_NAME}.framework/."
-if [ -f $IPHONEOS_FRAMEWORK_DIR ]; then
+IPHONEOS_FRAMEWORK_DIR="../build/Release-iphoneos/${SCHEMA_NAME}/${PRODUCT_NAME}.framework"
+if [ -d $IPHONEOS_FRAMEWORK_DIR ]; then
   mkdir -p "${OUTPUT_PATH}"
-  cp -a IPHONEOS_FRAMEWORK_DIR "${OUTPUT_PATH}"
+  cp -a $IPHONEOS_FRAMEWORK_DIR "${OUTPUT_PATH}"
 else
   echo "${IPHONEOS_FRAMEWORK_DIR} does not exist"
   exit 1
