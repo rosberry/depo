d1a48f959e43c15a6b824fc15c117ef402848731
zhvrnkov <vlad.zhavoronkov@rosberry.com>
2021-01-27 14:44:18 +0600

    check path existence in FileManager.perform

--- Sources/DepoCore/Helpers/FileManager+Helper.swift
+++ Sources/DepoCore/Helpers/FileManager+Helper.swift
@@ -6,8 +6,16 @@ import Foundation
 
 extension FileManager {
 
+    enum Error: Swift.Error {
+        case notExist(path: String)
+    }
+
     @discardableResult
-    func perform<T>(atPath path: String, _ action: () throws -> T) rethrows -> T {
+    func perform<T>(atPath path: String, _ action: () throws -> T) throws -> T {
+        var bool = ObjCBool(true)
+        guard fileExists(atPath: path, isDirectory: &bool) else {
+            throw Error.notExist(path: path)
+        }
         let currentPath = currentDirectoryPath
         defer {
             changeCurrentDirectoryPath(currentPath)
