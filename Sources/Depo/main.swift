4bc9f80b0768f46143eb68dc3b256ac3d62cf715
zhvrnkov <vlad.zhavoronkov@rosberry.com>
2021-01-28 13:07:17 +0600

    add CLT interface for Cacher

--- Sources/Depo/main.swift
+++ Sources/Depo/main.swift
@@ -4,6 +4,9 @@
 
 import Foundation
 import DepoCore
+import Files
+import ArgumentParser
+import PathKit
 
 var processes: [Process] = []
 
@@ -22,4 +25,111 @@ source.setEventHandler {
 }
 source.resume()
 
-Depo.main()
+// Depo.main()
+
+struct Cacher: ParsableCommand {
+
+    enum Error: Swift.Error {
+        case invalidURL(string: String)
+    }
+
+    struct Save: ParsableCommand {
+
+        @Option(name: .shortAndLong)
+        var gitRepoURL: String
+
+        @Option(name: .shortAndLong)
+        var buildURL: String
+
+        @Option(name: .shortAndLong)
+        var packageName: String
+
+        func run() throws {
+            let gitRepoURL = try Cacher.url(string: self.gitRepoURL)
+            let buildURL = try Cacher.url(string: Path(self.buildURL).absolute().string)
+            let cacher = GitCacher(gitRepoURL: gitRepoURL)
+            try cacher.save(buildURL: buildURL, packageID: .init(name: packageName))
+        }
+    }
+
+    struct Update: ParsableCommand {
+
+        @Option(name: .shortAndLong)
+        var gitRepoURL: String
+
+        @Option(name: .shortAndLong)
+        var buildURL: String
+
+        @Option(name: .shortAndLong)
+        var packageName: String
+
+        func run() throws {
+            let gitRepoURL = try Cacher.url(string: self.gitRepoURL)
+            let buildURL = try Cacher.url(string: Path(self.buildURL).absolute().string)
+            let cacher = GitCacher(gitRepoURL: gitRepoURL)
+            try cacher.update(buildURL: buildURL, packageID: .init(name: packageName))
+        }
+    }
+
+    struct Get: ParsableCommand {
+
+        @Option(name: .shortAndLong)
+        var gitRepoURL: String
+
+        @Option(name: .shortAndLong)
+        var packageName: String
+
+        func run() throws {
+            let gitRepoURL = try Cacher.url(string: self.gitRepoURL)
+            let cacher = GitCacher(gitRepoURL: gitRepoURL)
+            let url = try cacher.get(packageID: .init(name: packageName))
+            print(url)
+        }
+    }
+
+    struct Setup: ParsableCommand {
+
+        @Option(name: .shortAndLong)
+        var localGitRepoURL: String
+
+        @Option(name: .shortAndLong)
+        var remoteGitRepoURL: String?
+
+        func run() throws {
+            let localGitRepoURL = try Cacher.url(string: self.localGitRepoURL)
+            let remoteGitRepoURL = try? Cacher.url(string: self.remoteGitRepoURL ?? "")
+            let cacher = GitCacher(gitRepoURL: localGitRepoURL)
+            try cacher.setupRepository(at: localGitRepoURL, remoteURL: remoteGitRepoURL)
+        }
+    }
+
+    static let configuration: CommandConfiguration = .init(subcommands: [Save.self, Get.self, Setup.self, Update.self])
+
+    static func url(string: String) throws -> URL {
+        guard let url = URL(string: string) else {
+            throw Error.invalidURL(string: string)
+        }
+        return url
+    }
+}
+
+Cacher.main()
+
+//print(Folder.current.url.absoluteString)
+//print(try Folder.current.subfolder(at: "/Users").url.absoluteString)
+
+//let cacher = GitCacher(gitRepoURL: URL(string: "git@github.com:zhvrnkov/frameworks-store.git")!)
+//print(try cacher.get(packageID: .init(name: "Framezilla")))
+// try cacher.save(buildURL: URL(string: "/Users/vz/Developer/frameworks-store.bak/Framezilla.framework")!, packageID: .init(name: "Framezilla"))
+
+// print(try cacher.packageIDs())
+
+// try cacher.setupRepository(remoteURL: URL(string: "https://github.com/zhvrnkov/frameworks-store.git"))
+
+//let url = try cacher.get(packageID: .init(name: "Base-iOS"))
+//print(url)
+//
+//try cacher.save(buildURL: URL(string: "file:///Users/vz/Developer/Core-iOS/Carthage/Build/iOS/Framezilla.framework")!,
+//                packageID: .init(name: "Framezilla"))
+//
+// try cacher.delete(packageID: .init(name: "Framezilla"))
